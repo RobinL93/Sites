02817104ca117e0da4f8488f6f8e7d8664c772b6
RobinL93 <robin.larsson@infocaption.com>
2015-03-13 16:21:04 +0100

    Yo

---
+++ test/js/script.js
@@ -0,0 +1,8 @@
+$(function () {
+	updateLog();
+});
+
+function updateLog() {
+	$("#log").append("This is haxxxx This is haxxxx This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxx  This is haxxxxxxxx   <br />");
+	setTimeout("updateLog();", 4102300);
+}
