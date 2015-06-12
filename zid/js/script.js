b69de82cf34ee9900fa5371c21b9e4be342ce0ce
RobinL93 <robin.larsson@infocaption.com>
2015-06-12 15:28:53 +0200

    no message

--- zid/js/script.js
+++ zid/js/script.js
@@ -1,8 +1,19 @@
 $(function(){
 
-for (var i = 1; i < 6; i++) {
-	$('.sub-menu').append('<li><a href="">Game ' + i + '</a></li>');
-};
+	for (var i = 1; i < 6; i++) {
+		$('.sub-menu').append('<li><a href="">Game ' + i + '</a></li>');
+	};
+
+	if (isAndroidORiOS() != "unknown") {
+		console.log(isAndroidORiOS());
+		$(".sub-menu").remove();
+		$("#selectGame").append("<option>Game 1</option>");
+		$("#selectGame").append("<option>Game 2</option>");
+		$("#selectGame").append("<option>Game 3</option>");
+		$("#selectGame").append("<option>Game 4</option>");
+	}
+
+
 
 
 });
@@ -14,5 +25,18 @@ $(window).on("load resize", function(){
 function containerMarginToHeader(){
 	var height = $("#header").height();
 	$(".container").css("margin-top", ""+ (height + 30) +"px");
+}
+
+function isAndroidORiOS() {
+	var userAgent = navigator.userAgent;
+
+	if(userAgent.match(/iPhone/i) || userAgent.match(/iPod/i)){
+		return "iOS";
+	} else if(userAgent.match(/Android/i)){
+		return "Android";
+	} else {
+		return "unknown";
+	}
+
 }
 
